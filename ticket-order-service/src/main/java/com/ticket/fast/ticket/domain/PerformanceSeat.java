757b39052061dd5b00a2826c36add1532e4c1dbf
SangwooKim <rlatkedn2902@gmail.com>
2026-03-19 00:31:42 +0900

    Refactor: builder 생성자 추갸

--- ticket-order-service/src/main/java/com/ticket/fast/ticket/domain/PerformanceSeat.java
+++ ticket-order-service/src/main/java/com/ticket/fast/ticket/domain/PerformanceSeat.java
@@ -1,6 +1,8 @@
 package com.ticket.fast.ticket.domain;
 
+import com.ticket.fast.common.util.TsidUtil;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -13,4 +15,13 @@ public class PerformanceSeat {
     private String seatCode;
     private SeatStatus status;
     private Integer price;
+
+    @Builder
+    private PerformanceSeat(Long performanceId, String seatCode, SeatStatus status, Integer price) {
+        this.id = TsidUtil.nextLong();
+        this.performanceId = performanceId;
+        this.seatCode = seatCode;
+        this.status = status;
+        this.price = price;
+    }
 }
