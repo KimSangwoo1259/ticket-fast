843ee04c0fa0f6e2647cecde734cbed61f018767
SangwooKim <rlatkedn2902@gmail.com>
2026-03-20 14:05:28 +0900

    Refactor: bean scan 범위 재조정

--- ticket-order-service/src/main/java/com/ticket/fast/ticket/TicketOrderServiceApplication.java
+++ ticket-order-service/src/main/java/com/ticket/fast/ticket/TicketOrderServiceApplication.java
@@ -3,7 +3,7 @@ package com.ticket.fast.ticket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ticket.fast")
 public class TicketOrderServiceApplication {
 
     public static void main(String[] args) {
