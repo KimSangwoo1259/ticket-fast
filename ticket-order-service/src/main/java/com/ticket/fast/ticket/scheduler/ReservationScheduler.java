c76b7de606a108cf59d4ba4d90e8d93ba45ca4d3
SangwooKim <rlatkedn2902@gmail.com>
2026-04-28 18:00:20 +0900

    feat: 미결제 예약 정리 scheduler 부활

--- ticket-order-service/src/main/java/com/ticket/fast/ticket/scheduler/ReservationScheduler.java
+++ ticket-order-service/src/main/java/com/ticket/fast/ticket/scheduler/ReservationScheduler.java
@@ -14,16 +14,16 @@ public class ReservationScheduler {
 
     private final ReservationService reservationService;
 
-//    @Scheduled(fixedDelay = 60000) //1분 간격으로
-//    public void cleanupPendingReservations() {
-//        log.info("만료된 예약 정리 스케줄러 가동");
-//
-//        reservationService.processExpiredReservations()
-//                .subscribe(
-//                        null,
-//                        error -> log.error("스케줄러 작업 중 에러 발생: {}", error.getMessage(), error),
-//                        () -> log.info("만료된 예약 정리 완료")
-//                );
-//
-//    }
+    @Scheduled(fixedDelay = 60000) //1분 간격으로
+    public void cleanupPendingReservations() {
+        log.info("만료된 예약 정리 스케줄러 가동");
+
+        reservationService.processExpiredReservations()
+                .subscribe(
+                        null,
+                        error -> log.error("스케줄러 작업 중 에러 발생: {}", error.getMessage(), error),
+                        () -> log.info("만료된 예약 정리 완료")
+                );
+
+    }
 }
