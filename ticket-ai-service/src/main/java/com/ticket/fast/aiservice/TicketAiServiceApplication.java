4313cd0ae7a456291ecc173049abd3cb389affe1
SangwooKim <rlatkddn2902@gmail.com>
2026-05-21 14:44:06 +0900

    feat: google gemini api key 추가

--- ticket-ai-service/src/main/java/com/ticket/fast/aiservice/TicketAiServiceApplication.java
+++ ticket-ai-service/src/main/java/com/ticket/fast/aiservice/TicketAiServiceApplication.java
@@ -2,8 +2,13 @@ package com.ticket.fast.aiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class TicketAiServiceApplication {
 
     public static void main(String[] args) {
