54ff008423d49df0ee2e5905afbefb254001810e
SangwooKim <rlatkddn2902@gmail.com>
2026-06-02 15:18:10 +0900

    chore: 미사용 import 정리

--- ticket-order-service/src/main/java/com/ticket/fast/ticket/domain/Reservation.java
+++ ticket-order-service/src/main/java/com/ticket/fast/ticket/domain/Reservation.java
@@ -4,9 +4,7 @@ import com.ticket.fast.common.util.TsidUtil;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.annotation.Version;
-import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
