b5028b4af93e719da65c21353e54157799b837fc
SangwooKim <rlatkddn2902@gmail.com>
2026-06-30 15:20:43 +0900

    feat: event -> Reservation 변환 스태틱 메소드 작성

--- ticket-order-service/src/main/java/com/ticket/fast/ticket/dto/event/ReservationEvent.java
+++ ticket-order-service/src/main/java/com/ticket/fast/ticket/dto/event/ReservationEvent.java
@@ -1,5 +1,8 @@
 package com.ticket.fast.ticket.dto.event;
 
+import com.ticket.fast.ticket.domain.Reservation;
+import com.ticket.fast.ticket.domain.ReservationStatus;
+
 public record ReservationEvent(
         Long userId,
         Long performanceId,
@@ -8,4 +11,13 @@ public record ReservationEvent(
         Integer price
 
 ) {
+    public static Reservation toEntity(ReservationEvent event){
+        return Reservation.builder()
+                .performanceId(event.performanceId())
+                .userId(event.userId())
+                .seatCode(event.seatCode())
+                .price(event.price())
+                .status(ReservationStatus.PENDING)
+                .build();
+    }
 }
