72cc1cfbbc70870f44233c8b12f78ab81b1ef49c
SangwooKim <rlatkddn2902@gmail.com>
2026-06-30 15:20:19 +0900

    feat: batch consume 시 실패한 event 만 dlq 로 가고 나머지는 정상 작업할 수 있도록 변경

--- ticket-order-service/src/main/java/com/ticket/fast/ticket/consumer/ReservationConsumer.java
+++ ticket-order-service/src/main/java/com/ticket/fast/ticket/consumer/ReservationConsumer.java
@@ -1,11 +1,13 @@
 package com.ticket.fast.ticket.consumer;
 
+import com.ticket.fast.ticket.domain.Reservation;
 import com.ticket.fast.ticket.dto.event.ReservationEvent;
 import com.ticket.fast.ticket.repository.PerformanceSeatRepository;
 import com.ticket.fast.ticket.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.BatchListenerFailedException;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -25,6 +27,28 @@ public class ReservationConsumer {
             properties = {"max.poll.records=50"})
 
     public void consume(List<ReservationEvent> events){
+        try{
+            reservationRepository.saveAllEventsWithIgnore(events)
+                    .then(performanceSeatRepository.reserveSeatBulk(events.stream().map(ReservationEvent::performanceSeatId).toList()))
+                    .doOnError(e -> log.error("배치 저장중 에러 발생 {}", e.getMessage(), e))
+                    .block();
+        } catch (Exception bulkException){
+            log.warn("벌크 처리주 에러 발생, 순회하여 실패 데이터 탐색. Error {}",bulkException.getMessage());
+
+            for (int i = 0; i < events.size(); i++){
+                ReservationEvent event = events.get(i);
+                try {
+                    reservationRepository.save(ReservationEvent.toEntity(event))
+                            .then(performanceSeatRepository.reserveSeat(event.performanceSeatId()))
+                            .block();
+                } catch (Exception individualException){
+                    log.error("배치중 {}번째 데이터 에러 발생: {}", i, individualException.getMessage(), individualException);
+
+                    throw new BatchListenerFailedException("배치 내 특정 데이터 처리 실패", individualException,i);
+                }
+            }
+        }
+
 
         reservationRepository.saveAllEventsWithIgnore(events)
                 .then(performanceSeatRepository.reserveSeatBulk(events.stream().map(ReservationEvent::performanceSeatId).toList()))
