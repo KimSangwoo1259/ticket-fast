e479c28b4b78118b998255d3065d61df28a4c7e4
SangwooKim <rlatkddn2902@gmail.com>
2026-07-02 13:59:17 +0900

    feat: dlq와 일반 토픽 팩토리 분리

--- ticket-order-service/src/main/java/com/ticket/fast/ticket/config/KafkaConfig.java
+++ ticket-order-service/src/main/java/com/ticket/fast/ticket/config/KafkaConfig.java
@@ -1,17 +1,24 @@
 package com.ticket.fast.ticket.config;
 
 
+import com.ticket.fast.ticket.dto.event.ReservationEvent;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
-import org.springframework.kafka.support.converter.BatchMessagingMessageConverter;
-import org.springframework.kafka.support.converter.StringJacksonJsonMessageConverter;
+
+import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
 import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
@@ -31,23 +38,53 @@ public class KafkaConfig {
         return new DefaultErrorHandler(recoverer, fixedBackOff);
     }
 
-
-    // 배치 리스너 활성화
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
-            ConsumerFactory<String, Object> consumerFactory,
+    public ConsumerFactory<String, ReservationEvent> consumerFactory(
+            KafkaProperties kafkaProperties
+    ) {
+        Map<String, Object> props =
+                new HashMap<>(kafkaProperties.buildConsumerProperties());
+
+        JacksonJsonDeserializer<ReservationEvent> valueDeserializer =
+                new JacksonJsonDeserializer<>(ReservationEvent.class);
+
+        valueDeserializer.configure(Map.of(
+                JacksonJsonDeserializer.TRUSTED_PACKAGES,
+                "com.ticket.fast.ticket.dto.event"
+        ), false);
+
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                valueDeserializer
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, ReservationEvent> batchKafkaListenerContainerFactory(
+            ConsumerFactory<String, ReservationEvent> consumerFactory,
             DefaultErrorHandler errorHandler
     ) {
-
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-
+        ConcurrentKafkaListenerContainerFactory<String, ReservationEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
-
         factory.setCommonErrorHandler(errorHandler);
-
         factory.setBatchListener(true);
-        // JSON 문자열을 객체로 변환해 주는 배치 컨버터
-        factory.setBatchMessageConverter(new BatchMessagingMessageConverter(new StringJacksonJsonMessageConverter()));
+        return factory;
+    }
+
+    // 🏷️ 2. DLQ 토픽용 팩토리 (단건 리스너)
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, ReservationEvent> singleKafkaListenerContainerFactory(
+            ConsumerFactory<String, ReservationEvent> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, ReservationEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setBatchListener(false);
+
+
+        //DLQ 컨슈머에서 에러가 났을 때 또 .DLQ.DLQ로 가는 걸 막기 위해,
+        // 백오프(재시도)만 설정해 줍니다.
+        factory.setCommonErrorHandler(new DefaultErrorHandler(new FixedBackOff(1000L, 2)));
 
         return factory;
     }
